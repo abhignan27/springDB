2e2ef36a623cb4d06fa29afd441fcf4b549f0c3e
abhignan27 <abhignan27@gmail.com>
2024-06-26 00:52:30 +0100

    Added initial project files

---
+++ src/test/java/com/example/dbcrud/DbcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
